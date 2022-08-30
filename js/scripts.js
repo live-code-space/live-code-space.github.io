92128f3b347fafe86c0c349b2d6b6c6b301c6238
Paulina <hi@zybinska.io>
2022-08-30 17:34:17 +0200

    Setup

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,22 @@ $(document).ready(function() {
 });
 
 let interactive = new p5(s, 'p5lcs');
+
+mail_entry = document.getElementById("user_mail").value
+button_news = document.getElementById("button-newsletter")
+
+function sendEmail() {
+  Email.send({
+    Host: "",
+    Username: "sender@email_address.com",
+    Password: "Enter your password",
+    To: 'open.live.code@proton.me',
+    From: mail_entry,
+    Subject: "I want to receive newsletter",
+    Body: "Keeo me up to date!!",
+  })
+    .then(function () {
+      mail_entry = "Thank you!"
+      button_news.disabled = true;
+    });
+}
